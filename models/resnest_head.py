213d50119c0fd6d2058ce34bfc672cd6db93e99b
Viet Anh Nguyen <vietanh.dev@gmail.com>
2020-11-19 15:12:27 +0700

    Init

---
+++ models/resnest_head.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+class ResNeSt_head(nn.Module):
+    def __init__(self, pre_model):
+        super(ResNeSt_head, self).__init__()
+        self.pre_model = pre_model
+        self.last_conv = nn.Conv2d(1024, 21, (1,1), 1)
+        self.output = nn.Sigmoid()
+    def forward(self, x):
+        x = self.pre_model.conv1(x)
+        x = self.pre_model.bn1(x)
+        x = self.pre_model.relu(x)
+        x = self.pre_model.maxpool(x)
+        x = self.pre_model.layer1(x)
+        x = self.pre_model.layer2(x)
+        x = self.pre_model.layer3(x)
+        x = self.last_conv(x)
+        x = self.output(x)
+        return x
